28a902d54d082d9934dd9b9be4721dddb2dd1cda
Lucas <lucaswoodyard@yahoo.com>
2022-09-03 17:08:30 -0500

    Added button in HTML file

---
+++ index.js
@@ -0,0 +1,37 @@
+function getPokemon(pokemon) {
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=1&limit=150')
+    .then(res => res.json())
+    .then(data => data.pokeCard())
+    }
+
+
+
+
+
+
+
+
+
+// function pokeCard() {
+//     let p = document.createElement('p')
+//     let btn = document.createElement('button')
+//     btn.textContent = ' x'
+//     //p.textContent = `${searchUser} `
+//     p.appendChild(btn).addEventListener('click', deleteSearch)
+//     //let p1 = document.createElement('img')    
+    
+    
+//     document.querySelector('#user-list').appendChild(p) 
+//     let userInfo = getPokemon(event1)
+//     .then(res => res.json())
+//     .then(data => p1.src = (data.items[0].avatar_url))
+//     .catch(err => console.log(err))
+    // document.querySelector('#user-list').appendChild(p1) 
+    // p.addEventListener('click', (e) => {
+    //     fetchRepo(searchUser)
+        
+
+    // })
+
+    
+// }
